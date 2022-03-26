6cc5bfd53d9daf5b26ec8c14386c3890a248dfcc
Dania Syabitova <94595600+DaniaSyabitova@users.noreply.github.com>
2022-03-26 22:10:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,16 +12,15 @@ int countPairs1(int *arr, int len, int value) {
   return 0;
 }
 int countPairs2(int *arr, int len, int value) {
-  int i = 0, j = len - 1;
   int counter = 0;
-  if (arr[j] > value)
-    j--;
   for (i = 0; i < len - 1; i++) {
-    for (j = i + 1; j < len; j++) {
-      if (arr[i] + arr [j] == value)
-        counter++;
+    if (arr[i] <= value) {
+      for (j = len - 1; j > i; j--) {
+        if (arr[i] + arr[j] == value)
+          counter++;
+        }
+      }
     }
-  }
   if (counter != 0)
     return counter;
   return 0;
